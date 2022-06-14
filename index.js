2e7f383ba50dc222ab3e20892a2ac4e91b4e759a
ibrahmounk <myboxconn@gmail.com>
2022-06-14 23:08:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// $('h1').hide();
+// $('h1#heading1').hide();
+// $('.heading2').hide();
+// $('p span').css('color', 'blue');
+
+// $('ul#ulist li:first').css('color', 'red');
+// $('ul#ulist li:last').css('color', 'green')
+// $('ul#ulist li:nth-child(2n)').css('list-style', 'none')
+
+$('[href]').css('text-decoration', 'none');
+$('[href].c1').css('color', 'green');
+$('[href].c2').css('color', 'purple');
